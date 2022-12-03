6d87a952027fa1e01ab63a6b1854713c3e42d17d
Danikzx <danilsnigur21@gmail.com>
2022-12-03 20:53:14 +0200

    First commit

---
+++ Jiofoat/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jiofoat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+enum DataType
+{
+    INT16,
+    UINT16,
+    // И еще типы есть
+}
+
+interface IValueChangedEventArgsFactory
+{
+    IValueChangedEventArgs GetValueChangedEventArgs(string address, DataType dataType, OperateResult oldValue, OperateResult newValue);
+}
+
+class ValueChangedEventArgsFactory : IValueChangedEventArgsFactory
+{
+    private IDictionary<DataType, Func<string, DataType, OperateResult, OperateResult, IValueChangedEventArgs>> factory;
+
+    public ValueChangedEventArgsFactory()
+    {
+        factory = new Dictionary<DataType, Func<string, DataType, OperateResult, OperateResult, IValueChangedEventArgs>>()
+    {
+      { DataType.INT16, (string address, DataType dataType, OperateResult oldValue, OperateResult newValue)
+        => new ValueChangedEventArgs<short>(address, dataType, oldValue, newValue) },
+      { DataType.UINT16, (string address, DataType dataType, OperateResult oldValue, OperateResult newValue)
+        => new ValueChangedEventArgs<ushort>(address, dataType, oldValue, newValue) }
+    };
+    }
+
+    public IValueChangedEventArgs GetValueChangedEventArgs(string address, DataType dataType, OperateResult oldValue, OperateResult newValue)
+    {
+        factory.TryGetValue(dataType, out Func<string, DataType, OperateResult, OperateResult, IValueChangedEventArgs> func);
+        return (func == null) ? throw new NotImplementedException() : func.Invoke(address, dataType, oldValue, newValue);
+    }
+}
+
+interface IValueChangedEventArgs
+{
+    string Address { get; }
+    DataType DataType { get; }
+}
+
+interface IValueChangedEventArgs<T> : IValueChangedEventArgs
+{
+    OperateResult<T> NewValue { get; }
+    OperateResult<T> OldValue { get; }
+}
+
+class ValueChangedEventArgs<T> : System.EventArgs, IValueChangedEventArgs<T>
+{
+    public string Address { get; private set; }
+    public DataType DataType { get; private set; }
+    public OperateResult<T> OldValue { get; private set; }
+    public OperateResult<T> NewValue { get; private set; }
+
+    public ValueChangedEventArgs(string address, DataType dataType, OperateResult oldValue, OperateResult newValue)
+    {
+        Address = address;
+        DataType = dataType;
+        OldValue = (OperateResult<T>)oldValue;
+        NewValue = (OperateResult<T>)newValue;
+    }
+}
+
+class OperateResult
+{
+    public bool IsSuccess;
+}
+
+class OperateResult<T> : OperateResult
+{
+    public T Content;
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var factory = new ValueChangedEventArgsFactory();
+        var oldValue = new OperateResult<short>() { Content = 1 };
+        var newValue = new OperateResult<short>() { Content = 2 };
+
+        var args = factory.GetValueChangedEventArgs(string.Empty, DataType.INT16, oldValue, newValue);
+    }
+}
---
+++ Scorilenos/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scorilenos
+{
+        public interface WatchingFilms
+
+        {
+            IEnumerable<WatchingItem> GetAllElements();
+            WatchingItem GetElementById(Guid id);
+           
+        }
+    
+}
+
+
+public enum day
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+}
+
+
+public enum genrefilm
+{
+    Romance,
+    Comedy,
+    Drama,
+    Fantasy,
+    Adventure,
+    
+}
+
+public enum dayTimeviewing
+{
+    ten = 10,
+    twelve = 12,
+    fourteen = 14,
+}
+
+
+public enum eveningTimeviewing
+{
+    sixteen = 16,
+    eighteen = 18,
+    twenty = 20,
+    twentytwo = 22,
+}
+
+public class WatchingItem
+{
+    public Guid Id { get; set; }
+    public timeOfDay TimeOfDay { get; set; }    
+    public day Day { get; set; }
+    public dayTimeviewing dayTimeviewing { get; set; }
+    public eveningTimeviewing eveningTimeviewing { get; set; }
+    public int Romance { get; set; }
+    public int Comedy { get; set; } 
+    public int Drama { get; set; }
+    public int Fantasy { get; set; }
+    public int Adventure { get; set; }
+    public int viewingDayTime => 3 + (int)(viewingDayTime / 0.8889);
+    public int viewingEveningTime => 4 + (int)(viewingEveningTime / 0.8889);
+    public string viewingChance { get; set; }
+
+}
+
+
+public WatchingItem GetElementById(Guid id, int viewingDayTime)
+{
+    var rng = new Random();
+    WatchingItem r = new WatchingItem
+    {
+        Id = id,
+        TimeOfDay = TimeOfDayRandom(),
+        Day = DayRandom(),
+        dayTimeviewing = dayTimeviewingRandom(),
+        eveningTimeviewing = eveningTimeviewingRandom(),
+        Romance = RomanceRandom(),
+        Comedy = ComedyRandom(),
+        Drama = DramaRandom(),
+        Fantasy = FantasyRandom(),
+        Adventure = AdventureRandom(),
+        viewingDayTime = rng.Next(1, 3),
+        viewingEveningTime = rng.Next(1, 4),
+        viewingChance = ViewingChanceRandom()
+    };
+
+    return r;
+}
+
+public string CombineIntegers(int a, int b) => a + "." + b;
+
+int AdventureRandom()
+{
+    throw new NotImplementedException();
+}
+
+string ViewingChanceRandom()
+{
+    throw new NotImplementedException();
+}
+
+int FantasyRandom()
+{
+    throw new NotImplementedException();
+}
+
+timeOfDay TimeOfDayRandom()
+{
+    throw new NotImplementedException();
+}
+
+day DayRandom()
+{
+    throw new NotImplementedException();
+}
+
+dayTimeviewing dayTimeviewingRandom()
+{
+    throw new NotImplementedException();
+}
+
+eveningTimeviewing eveningTimeviewingRandom()
+{
+    throw new NotImplementedException();
+}
+
+int RomanceRandom()
+{
+    throw new NotImplementedException();
+}
+
+int ComedyRandom()
+{
+    throw new NotImplementedException();
+}
+
+int DramaRandom()
+{
+    throw new NotImplementedException();
+}
+
+
